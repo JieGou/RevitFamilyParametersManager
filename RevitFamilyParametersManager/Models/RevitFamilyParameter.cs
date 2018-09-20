49a535afd5a023726a89b476d2ad63744ac8643d
psv <spavlovitsky@gmail.com>
2018-09-20 17:48:06 +0300

    up

--- RevitFamilyParametersManager/Models/RevitFamilyParameter.cs
+++ RevitFamilyParametersManager/Models/RevitFamilyParameter.cs
@@ -3,15 +3,49 @@ using RevitFamilyParametersManager.Interfaces;
 using RevitPSVUtils;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace RevitFamilyParametersManager.Models
 {
     public class RevitFamilyParameter : IParameter
     {
-        public string Name { get; set; }
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+
+                if (Document == null)
+                    return;
+
+                if (this.Model.IsBuiltInParam())
+                {
+                    return;
+                }
+
+                
+
+                if(!this.Model.IsShared)
+                {
+                    using (var t = new Autodesk.Revit.DB.Transaction(Document, "Set parameters to all types"))
+                    {
+                        t.Start();
+                        this.FamilyManager.RenameParameter(this.Model, _name);
+                        t.Commit();
+                    }
+                }
+                
+            }
+        }
 
         private string _value;
         public string Value
@@ -22,13 +56,15 @@ namespace RevitFamilyParametersManager.Models
             }
             set
             {
-
+                if (Document == null || this.Model.IsReadOnly || !this.Model.UserModifiable)
+                    return;
                 _value = value;
                 foreach (FamilyType famType in this.FamilyManager.Types)
                 {
                     //Устанавливаем текущий тип для изменения
                     if (this.StorageType == StorageType.Double)
                     {
+
                         using (var t = new Autodesk.Revit.DB.Transaction(Document, "Set parameters to all types"))
                         {
                             t.Start();
@@ -61,7 +97,33 @@ namespace RevitFamilyParametersManager.Models
                 }
             }
         }
-        public bool IsInstance { get; set; }
+
+        private bool _isInstance = false;
+        public bool IsInstance
+        {
+            get
+            {
+                return _isInstance;
+            }
+            set
+            {
+                if (Document == null)
+                    return;
+                if (this.Model.IsBuiltInParam())
+                    return;
+
+                _isInstance = value;
+                using (var t = new Autodesk.Revit.DB.Transaction(Document, "Set parameters to all types"))
+                {
+                    t.Start();
+                    if (IsInstance == true)
+                        this.FamilyManager.MakeInstance(this.Model);
+                    else
+                        this.FamilyManager.MakeType(this.Model);
+                    t.Commit();
+                }
+            }
+        }
         public bool IsShared { get; }
         public string Formula { get; set; }
         public StorageType StorageType { get; set; }
@@ -73,14 +135,15 @@ namespace RevitFamilyParametersManager.Models
         {
             //revitFamParams.Add(new RevitFamilyParameter(familyParam.Definition.Name, familyParam.IsInstance,
             //                                                familyParam.IsShared, familyParam.Formula, familyParam.DisplayUnitType));
+            Document = doc;
+            Model = familyParam;
+            FamilyManager = familyManager;
             Name = familyParam.Definition.Name;
             IsInstance = familyParam.IsInstance;
             IsShared = familyParam.IsShared;
             Formula = familyParam.Formula;
             StorageType = familyParam.StorageType;
-            Model = familyParam;
-            FamilyManager = familyManager;
-            Document = doc;
+
 
         }
     }
